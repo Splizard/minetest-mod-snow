337e9e2c55d1a038f6618278a7da71470fa4f717
HybridDog <ovvv@web.de>
2018-08-07 15:39:28 +0200

    Fix default settings
    Some boolean settings disappeared in https://github.com/Splizard/minetest-mod-snow/commit/0c90b5dcf30c9f85b4030dd05b12f45427426cd1

--- src/util.lua
+++ src/util.lua
@@ -1,35 +1,20 @@
 --Global config and function table.
 snow = {
-	snowball_gravity = tonumber(minetest.settings:get("snowball_gravity")) or 0.91,
-	snowball_velocity = tonumber(minetest.settings:get("snowball_velocity")) or 19,
-	sleds = minetest.settings:get_bool("sleds"),
-	enable_snowfall = minetest.settings:get_bool("enable_snowfall"),
-	lighter_snowfall = minetest.settings:get_bool("lighter_snowfall"),
-	debug = minetest.settings:get_bool("debug"),
-	smooth_biomes = minetest.settings:get_bool("smooth_biomes"),
-	christmas_content = minetest.settings:get_bool("christmas_content"),
-	smooth_snow = minetest.settings:get_bool("smooth_snow"),
-	min_height = tonumber(minetest.settings:get("min_height")) or 3,
-	mapgen_rarity = tonumber(minetest.settings:get("mapgen_rarity")) or 18,
-	mapgen_size = tonumber(minetest.settings:get("mapgen_size")) or 210,
-	disable_mapgen =  minetest.settings:get_bool("disable_mapgen"),
+	snowball_gravity = tonumber(minetest.settings:get("snow_snowball_gravity")) or 0.91,
+	snowball_velocity = tonumber(minetest.settings:get("snow_snowball_velocity")) or 19,
+	sleds = minetest.settings:get_bool("snow_sleds", true),
+	enable_snowfall = minetest.settings:get_bool("snow_enable_snowfall", true),
+	lighter_snowfall = minetest.settings:get_bool("snow_lighter_snowfall", false),
+	debug = minetest.settings:get_bool("snow_debug", false),
+	smooth_biomes = minetest.settings:get_bool("snow_smooth_biomes", true),
+	christmas_content = minetest.settings:get_bool("snow_christmas_content", true),
+	smooth_snow = minetest.settings:get_bool("snow_smooth_snow", true),
+	min_height = tonumber(minetest.settings:get("snow_min_height")) or 3,
+	mapgen_rarity = tonumber(minetest.settings:get("snow_mapgen_rarity")) or 18,
+	mapgen_size = tonumber(minetest.settings:get("snow_mapgen_size")) or 210,
+	disable_mapgen =  minetest.settings:get_bool("snow_disable_mapgen", true),
 }
 
-if snow.sleds == nil then
-	snow.sleds = true
-end
-
-if snow.debug == nil then
-	snow.debug = false
-end
-
-if snow.disable_mapgen == nil then
-	snow.disable_mapgen = true
-end
-
-if snow.enable_snowfall == nil then
-	snow.enable_snowfall = true
-end
 
 -- functions for dynamically changing settings
 
