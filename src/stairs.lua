63d702058501a3e56bf0478de94ee34db000800b
HybridDog <ovvv@web.de>
2026-01-25 13:40:02 +0100

    Fix ice and snowblock stairs warnings if there's no stairsplus mod
    The stairs mod in minetest_game already registers ice and snowblock
    stairs and warns now about the re-registration in the snow mod.
    
    After this change, for the case without moreblocks and stairsplus,
    we override stair nodes directly with minetest.override_item().

--- src/stairs.lua
+++ src/stairs.lua
@@ -69,33 +69,62 @@ if minetest.get_modpath("moreblocks") and
 	end
 
 elseif minetest.global_exists("stairs") then -- simple stairs and slabs only
-	for mod, nodes in pairs(snow_nodes) do
-		for _, name in pairs(nodes) do
-			local nodename   = mod .. ":" .. name
-			local ndef = table.copy(minetest.registered_nodes[nodename])
 
-			local desc_stair = ndef.description .. " Stair"
-			local desc_slab  = ndef.description .. " Slab"
-			local images = ndef.tiles
-			local sounds = ndef.sounds
+	local stair_prefixes = {"stairs:slab_", "stairs:stair_",
+		"stairs:stair_inner_", "stairs:stair_outer_"}
+	for i = 1, #stair_prefixes do
+		local nodename = stair_prefixes[i] .. "ice"
+		local groups = table.copy(minetest.registered_nodes[nodename].groups)
+		-- melt probably does nothing but default:ice also has it
+		groups.melt = 1
+		groups.melts = 2
+		minetest.override_item(nodename, {
+			groups = groups,
+			sunlight_propagates = true,
+			tiles = {
+				{
+					align_style = "world",
+					backface_culling = true,
+					name = "snow_ice.png^[brighten",
+				},
+			},
+			use_texture_alpha = "blend",
+		})
 
-			local groups = ndef.groups
-			groups.melts = 2
-			groups.icemaker = nil
-			groups.cooks_into_ice = nil
+		nodename = stair_prefixes[i] .. "snowblock"
+		groups = table.copy(minetest.registered_nodes[nodename].groups)
+		groups.melts = 2
+		groups.falling_node = 1
+		minetest.override_item(nodename, {
+			groups = groups
+		})
+	end
 
-			stairs.register_stair_and_slab(name, nodename,
-				groups, images, desc_stair, desc_slab, sounds)
+	for _, name in pairs(snow_nodes.snow) do
+		local nodename = "snow:" .. name
+		local ndef = table.copy(minetest.registered_nodes[nodename])
 
-			-- Add transparency if used (e.g. ice and ice_brick).
-			minetest.override_item("stairs:stair_" .. name,
-				{use_texture_alpha = ndef.use_texture_alpha})
-			minetest.override_item("stairs:slab_"  .. name,
-				{use_texture_alpha = ndef.use_texture_alpha})
+		local desc_stair = ndef.description .. " Stair"
+		local desc_slab  = ndef.description .. " Slab"
+		local images = ndef.tiles
+		local sounds = ndef.sounds
 
-			-- Alias all stairs and slabs from snow to the stairs namespace.
-			minetest.register_alias("snow:stair_" .. name, "stairs:stair_" .. name)
-			minetest.register_alias("snow:slab_"  .. name, "stairs:slab_"  .. name)
-		end
+		local groups = ndef.groups
+		groups.melts = 2
+		groups.icemaker = nil
+		groups.cooks_into_ice = nil
+
+		stairs.register_stair_and_slab(name, nodename,
+			groups, images, desc_stair, desc_slab, sounds)
+
+		-- Add transparency if used (ice_brick).
+		minetest.override_item("stairs:stair_" .. name,
+			{use_texture_alpha = ndef.use_texture_alpha})
+		minetest.override_item("stairs:slab_"  .. name,
+			{use_texture_alpha = ndef.use_texture_alpha})
+
+		-- Alias all stairs and slabs from snow to the stairs namespace.
+		minetest.register_alias("snow:stair_" .. name, "stairs:stair_" .. name)
+		minetest.register_alias("snow:slab_"  .. name, "stairs:slab_"  .. name)
 	end
 end
